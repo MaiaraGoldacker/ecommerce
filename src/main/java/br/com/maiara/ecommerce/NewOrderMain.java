3874838261b82c7642a2e579014a288c59e0232e
MaiaraGoldackerPetlove <maiara.goldacker@petlove.com.br>
2021-08-10 09:08:05 -0300

    Consumidores genéricos

--- src/main/java/br/com/maiara/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/maiara/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.maiara.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,16 +13,23 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
        var producer = new KafkaProducer<String, String>(properties()); //configurações iniciais de conexão com kafka
-        var value = "123, 8787878788788, 123456"; //mensagem
+       var value = "123, 8787878788788, 123456"; //mensagem
        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value); //configurações de envio de mensagem
-        producer.send(record, (data, ex) -> {
+
+       Callback  callback = (data, ex) -> {
             if (ex != null){
                 ex.printStackTrace();
                 return;
             }
             //Isso será um observer, quando terminar, irá printar as informações em console
             System.out.println("Sucesso" + data.topic() + ":::partition: " + data.partition() + "/offset: " + data.offset() + "/timestamp: " + data.timestamp());
-        } ).get(); //método assíncrono, por isso o get() -> ele espera o método terminar.
+        };
+
+       var email = "welcome!";
+       var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+
+       producer.send(record, callback).get(); //método assíncrono, por isso o get() -> ele espera o método terminar.
+       producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties(){
